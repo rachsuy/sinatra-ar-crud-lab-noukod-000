84404bf9040bc1c1e663df2acdbd80f94bd9324c
Rachele Suy <rachelesuy@gmail.com>
2019-08-20 20:00:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,6 +8,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    # erb :index
   end
+
+  # Allow user to create a new Post
+  get '/posts/new' do
+    erb :new
+  end
+  
+   # this is where we make new posts that save to our database!
+  post '/posts' do
+    Post.create(params)
+    redirect '/posts'
+  end
+  
+   # This allows us to see our posts
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  
+   # This allows to get the posts from the database
+   get '/posts/:id' do
+     @post = Post.find(params["id"])
+     erb :show
+   end
+   
+   # Allow to update the Record
+   get '/posts/:id/edit' do
+     @post = Post.find(params["id"])
+     erb :edit
+   end
+   
+    # Allow to update the Record in the database
+    patch '/posts/:id' do
+      Post.update(params[:id], name: params[:name], content: params[:content])
+      @post = Post.find(params[:id])
+
+      erb :show
+    end
+
+    # Allow to delete a Record
+    delete '/posts/:id/delete' do
+      Post.delete params[:id]
+      redirect '/posts'
+    end
+
 end
